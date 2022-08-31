9708074d005be9aab4b2801618223ef46e182fe3
Amal Syed <raptorshots@gmail.com>
2022-08-31 20:59:43 +0530

    added user

--- djangoProject/djangoapi/models.py
+++ djangoProject/djangoapi/models.py
@@ -20,7 +20,7 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
-        user = self.create_User(email, name, password)
+        user = self.create_user(email, name, password)
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
